429c892c78155dc54a73fee0e8300b20b937ee39
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2020-05-05 10:08:13 +0200

    fix scroll function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-$(window).scroll(function () {
-    var top = $(window).scrollTop();
-    if (top < 200) {
+function scrollPosition(){
+    var scroll = window.scrollY;
+    if (scroll < 200) {
         $('.nav-fixed').addClass('nav-fixed--bigger');
         $('#scroll').show();
     } else {
         $('.nav-fixed').removeClass('nav-fixed--bigger');
         $('#scroll').hide();
     }
-});
+}
+document.addEventListener("DOMContentLoaded", scrollPosition);
+window.addEventListener('scroll', scrollPosition);
 
 lightGallery(document.getElementById('lightgallery')); 
-
 
 const mainNav = document.getElementById('main-nav');
 const navBar = document.getElementById('nav-bar')
